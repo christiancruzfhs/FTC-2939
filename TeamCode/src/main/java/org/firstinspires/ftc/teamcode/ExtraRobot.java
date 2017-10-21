99674e4fb1cd79c4424da92979492a8d71440edf
christiancruzfhs <christian.c1@live.com>
2017-10-21 13:21:06 -0700

    Adding limit switch for elevator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtraRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExtraRobot.java
@@ -4,7 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 /**
@@ -12,6 +14,8 @@ import com.qualcomm.robotcore.util.Range;
  */
 @TeleOp(name="ExtraRobot", group="testing")
 public class ExtraRobot extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     private DcMotor elevatorMotor;
 
@@ -21,6 +25,9 @@ public class ExtraRobot extends LinearOpMode {
     private DcMotor leftMotor;
     private DcMotor rightMotor;
 
+    private DigitalChannel topLimitSwitch;
+    private DigitalChannel bottomLimitSwitch;
+
     static final double INCREMENT = 0.01;
     static final double MAX_POS = 1.0;
     static final double MIN_POS = 0.0;
@@ -29,28 +36,38 @@ public class ExtraRobot extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
         elevatorMotor = hardwareMap.get(DcMotor.class,"elevator_motor");
         leftGripper = hardwareMap.get(Servo.class,"left_gripper");
         rightGripper = hardwareMap.get(Servo.class,"right_gripper");
         leftMotor = hardwareMap.get(DcMotor.class,"left_motor");
         rightMotor = hardwareMap.get(DcMotor.class,"right_motor");
+        topLimitSwitch = hardwareMap.get(DigitalChannel.class,"top_limit_switch");
+        bottomLimitSwitch = hardwareMap.get(DigitalChannel.class,"bottom_limit_switch");
 
         leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightGripper.setDirection(Servo.Direction.REVERSE);
 
+        topLimitSwitch.setMode(DigitalChannel.Mode.INPUT);
+        bottomLimitSwitch.setMode(DigitalChannel.Mode.INPUT);
+
         waitForStart();
+        runtime.reset();
 
         boolean precisionMode = false;
 
         while (opModeIsActive()){
             if (gamepad1.y) {
                 precisionMode = !precisionMode;
+
             }
 
-            if (gamepad1.dpad_up){
+            if (gamepad1.dpad_up && !topLimitSwitch.getState()){
                 elevatorMotor.setPower(-1);
-            } else if (gamepad1.dpad_down){
+            } else if (gamepad1.dpad_down && !bottomLimitSwitch.getState()){
                 elevatorMotor.setPower(1);
             } else {
                 elevatorMotor.setPower(0);
@@ -82,6 +99,10 @@ public class ExtraRobot extends LinearOpMode {
 
             leftMotor.setPower(leftPower);
             rightMotor.setPower(rightPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
         }
     }
 }
